c67d3b4ed0c348f02cbd3b5da4f8f35776e0a01d
Alexander Kucherenko <alex.kucherenko@xakep.ru>
2019-11-13 11:05:04 +0300

    Model for jira configuration and updated config file
    Added support for components and labels

---
+++ Models/JiraConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kaiten.Models
+{
+  class JiraConfiguration
+  {
+    public String ProjectName { get; set; }
+    public String Password { get; set; }
+    public String Login { get; set; }
+    public String Url { get; set; }
+    public String ParentId { get; set; }
+    public String UserName { get; set; }
+    public String[] Components { get; set; }
+    public String[] Labels { get; set; }
+  }
+}
